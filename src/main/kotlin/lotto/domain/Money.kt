8a3697abe234b5b99592be0cef0f13c708fb9d09
yoonnyeong(최윤녕) <choiyn1994@naver.com>
2023-11-26 09:44:25 +0900

    fix : LottoPrize내 속성이 람다식가지도록 수정
    - LottoResult내 result 생성자파라미터로 넣어주도록 수정

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -1,8 +1,6 @@
 package lotto.domain
 
 class Money(val amount: Int) {
-    private val LOTTO_PRICE = 1000
-
     fun getCounts(): Int {
         return amount / LOTTO_PRICE
     }
@@ -11,5 +9,9 @@ class Money(val amount: Int) {
         require(amount > 0 && amount % LOTTO_PRICE == 0) {
             "구매금액을 정확히 입력해주세요. 로또 한장 가격은 $LOTTO_PRICE 원입니다."
         }
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
     }
 }
